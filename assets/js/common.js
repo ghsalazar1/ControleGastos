d4a58ba55595d9e00a414d89b8020893495afbfd
Gleybe Hebson Salazar Cavalcanti <gleybe.cavalcanti@vectracs.com.br>
2022-06-13 13:10:12 -0300

    [DEV] - adicionando swal nas telas ultimas modificações

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $(".list-group-item").click(function(e){
-        var openWarning = e.target.attributes['data-open-warning'];
-        openWarning = (openWarning == undefined) ? e.target.parentElement.attributes['data-open-warning'] : openWarning;
+        var openDangerModal = e.target.attributes['data-open-warning'];
+        openDangerModal = (openDangerModal == undefined) ? e.target.parentElement.attributes['data-open-warning'] : openDangerModal;
 
-        if(openWarning != undefined && openWarning != null && openWarning.textContent == 'true'){
+        if(openDangerModal != undefined && openDangerModal != null && openDangerModal.textContent == 'true'){
             e.preventDefault();
             Swal.fire({
                 title: 'Confirmação',
@@ -42,6 +42,22 @@ $(document).ready(function() {
               window.location = evt.target.attributes.href.value;
             }
           });
+    });
+
+    $(".user-register").click(function(e){
+      var evt = e
+      e.preventDefault();
+      Swal.fire({
+          title: 'Confirmado!',
+          text: '',
+          icon: 'success',
+          confirmButtonColor: '#3fa906',
+          allowOutsideClick: false
+        }).then((result)=> {
+          if (result.isConfirmed) {
+            window.location = evt.target.attributes.href.value;
+          }
+        });
     });
 
     $(".success-edit-swal").click(function(e){
